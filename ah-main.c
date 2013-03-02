cb86989373cc9f61ecf51d6e272298c6696b25c1
Ramon <oni.pool@gmail.com>
2013-03-02 19:03:25 +0200

    Added Makefile for MacOSX and fix library paths

--- ah-main.c
+++ ah-main.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <GL/glew.h>
-#include <GL/glut.h>
+#ifdef __APPLE__
+#    include <GLUT/glut.h>
+#else
+#    include <GL/glut.h>
+#endif
 #include <math.h>
 #include <stdio.h>
 #include "ah-util.h"

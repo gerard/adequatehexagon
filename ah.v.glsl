51db51095e2f2ec0deabbbc3b4e7cd3f0da57af0
Gerard Lledo <gerard.lledo@gmail.com>
2013-03-02 21:15:49 +0200

    Cleanup old texture code

--- ah.v.glsl
+++ ah.v.glsl
@@ -3,8 +3,6 @@
 attribute vec2 position;
 uniform float fade_factor;
 
-varying vec2 texcoord;
-
 void main()
 {
     float rot_factor = 3.141592 * 2.0 * fade_factor;
@@ -13,5 +11,4 @@ void main()
         vec2(-sin(rot_factor), cos(rot_factor))
     );
     gl_Position = vec4(rotation * position * vec2(0.5), 0.0, 1.0);
-    texcoord = position * vec2(0.5) + vec2(0.5);
 }
